6d79bac4f5845839b6f9dcd7fabb32c500f702f5
Miiro <NicknameAlwaystaken@users.noreply.github.com>
2023-03-26 22:19:45 +0300

    deleting unused code

--- Scripts/discord-bot.py
+++ Scripts/discord-bot.py
@@ -10,10 +10,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 prefix = '!'
 
-class MyClient(discord.Client):
-  async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
-
 intents = discord.Intents.default()
 intents.message_content = True
 
