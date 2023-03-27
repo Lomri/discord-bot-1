3d0352c79bc5c8c427eea2234d51b03de0bb7719
Miiro <NicknameAlwaystaken@users.noreply.github.com>
2023-03-27 17:07:07 +0300

    going from setting up server to checking it

--- Scripts/server_setup.py
+++ Scripts/server_setup.py
@@ -1,7 +1,10 @@
 from discord.ext import commands
+import discord
 from functions import set_role
+from functions import set_main_category
 
 signup_role_name = 'Player'
+player_field_category_name = 'Player Field'
 
 class Setup_events(commands.Cog):
     def __init__(self, bot):
@@ -12,34 +15,44 @@ class Setup_events(commands.Cog):
 
       print("Initializing")
 
-      signup_role = None
-
       async for guild in self.bot.fetch_guilds(limit=150):
         #Fetch all servers, add Player role if it's missing and add it to global variable
 
         print(guild)
 
-        found_signup_role = False
+        signup_role = None
+        main_category = None
+
         role_list = await guild.fetch_roles()
+        category_list = guild.categories
 
         for role in role_list:
           print(role)
           if(role.name == signup_role_name):
             #role found
 
-            found_signup_role = True
             signup_role = role
 
             print("Player role found!")
             break
 
-        if(not found_signup_role):
-          #role not found
-          print("Player role created!")
-          signup_role = (await guild.create_role(name=signup_role_name))
-
         #functions.py setting role there for use in functions
         set_role(signup_role)
+        
+        for category in category_list:
+          #Checks for categories and if main category exists
+          print(category)
+
+          if(category.name == player_field_category_name):
+            #category found
+
+            main_category = category
+
+            print("Main category found!")
+            break
+
+        #functions.py setting main category there for use in functions
+        set_main_category(main_category)
 
 
 
